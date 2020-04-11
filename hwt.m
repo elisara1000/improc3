dc7f4866fcb89bb5bb3fb88a6c408fe26d3b98b5
elisara1000 <elisara1000@hotmail.com>
2020-04-11 17:05:19 +0100

    inverse but doesnt work right

--- hwt.m
+++ hwt.m
@@ -3,25 +3,57 @@ close all; clear; clc;
 im = im2double(imread('cameraman.tif'));
 [M, N] = size(im);
 
-tr = zeros(M,N)  % transform of image
+tr = im;  % transform of image
 
 % transform rows (iteration 1)
 for i = 1: N  
     for k = 1 : N/2
-        tr(i,k) = (im(i, 2*k-1) + im(i, 2*k))/2; % average
-        tr(i,(N/2)+k) = (im(i, 2*k-1) - im(i, 2*k))/2; 
+        tr(i,k) = (tr(i, 2*k-1) + tr(i, 2*k))/2; % average
+        tr(i,(N/2)+k) = (tr(i, 2*k-1) - tr(i, 2*k))/2; 
     end
 end
 
 % transform half columns (iteration 2)
-for j = 1: M  
+for j = 1: M
     for k = 1 : M/2
-        tr(j,k) = (tr(j, 2*k-1) + tr(j, 2*k))/2; % average
-        tr(j,(N/2)+k) = (tr(j, 2*k-1) - tr(j, 2*k))/2; 
+        tr(k, j) = (tr(j, 2*k-1) + tr(j, 2*k))/2; % average
+        tr((M/2)+k, j) = (tr(j, 2*k-1) - tr(j, 2*k))/2; 
     end
 end
 
 figure, imshow(tr)
+
+inv = tr;
+
+% inverse
+for j = 1 : M
+    pos = 1;
+    for k = 1 : M/2      
+        s = tr(k, j);
+        d = tr((M/2)+k, j);
+        
+        inv(pos, j) = s + d;
+        inv(pos+1, j) = s + d;
+        
+        pos = pos + 2;
+    end
+end
+
+% rows
+for i = 1 : N
+    pos = 1;
+    for k = 1 : N/2      
+        s = tr(i, k);
+        d = tr(i, (N/2)+k);
+        
+        inv(i, pos) = s + d;
+        inv(i, pos+1) = s + d;
+        
+        pos = pos + 2;
+    end
+end
+
+figure, imshow(inv)
 % inverse transform
 
 
