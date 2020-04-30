bc120f00d71ba3a97683e0f20fbdbba39d272a47
Conor <conor.obrien24@gmail.com>
2020-04-30 13:18:12 +0100

    fixed and cleaned part2a

--- part2a.m
+++ part2a.m
@@ -1,31 +1,100 @@
-Q = 25; % Define quality factor
+clear all; close all; clc;
 
-% Define base quantization matrix
-Tb = [16 11 10 16 24 40 51 61; 12 12 14 19 26 58 60 55; ...
-     14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62; ...
-     18 22 37 56 68 109 103 77; 24 35 55 64 81 104 113 92; ...
-     49 64 78 87 103 121 120 101; 72 92 95 98 112 100 103 99];
+im_uint8 = imread('cameraman.tif');
 
-% Determine S
-if (Q < 50)
-    S = 5000/Q;
-else
-    S = 200 - 2*Q;
-end
+figure
+imshow(im_uint8);
+title("original image");
+figure
 
-Ts = floor((S*Tb + 50) / 100);
-Ts(Ts == 0) = 1; % Prevent division by 0
+%% Define several quantization matrices
+%Quantization matrix specified in JPEG spec for 50% quality
+qmat50 = [16 11 10 16 24 40 51 61; 12 12 14 19 26 58 60 55;...
+    14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62; ...
+    18 22 37 56 68 109 103 77; 24 35 55 64 81 104 113 92;...
+    49 64 78 87 103 121 120 101; 72 92 95 98 112 100 103 99];
+qmat_const_50 = round(ones([8 8]) * (sum(qmat50(:))/numel(qmat50(:))));
+
+%make a lower quality quant matrix
+qmat25 = make_qmat(25,qmat50);
+qmat_const_25 = round(ones([8 8]) * (sum(qmat25(:))/numel(qmat25(:))));
+
+%make a higher quality quant matrix
+qmat98 = make_qmat(98,qmat50);
+qmat_const_98 = round(ones([8 8]) * (sum(qmat98(:))/numel(qmat98(:))));
+
+%% jpeg/inverse jpeg using quality 25 quantization matrix & constant equivalent
+huff = jpeg(im_uint8, qmat25);
+q25 = ijpeg(huff, qmat25);
+
+err = immse(q25, im_uint8); % mean square error
+peaksnr = psnr(q25, im_uint8); % PSNR
+fprintf("Quality 25: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,1);
+imshow(q25,'Border','tight');
+title(["Quality 25, SNR:" num2str(peaksnr)]);
+
+huff = jpeg(im_uint8, qmat_const_25);
+q_const_25 = ijpeg(huff, qmat_const_25);
+
+err = immse(q_const_25, im_uint8); % mean square error
+peaksnr = psnr(q_const_25, im_uint8); % PSNR
+fprintf("Const 25: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,2);
+imshow(q_const_25,'Border','tight');
+title(["Q25 constant equivalent, SNR:" num2str(peaksnr)]);
 
 
-im = imread('cameraman.tif');
 
-huff = jpeg(im, Ts);
-quant = ijpeg(huff, Ts);
 
-figure; imshow(quant)
-err = immse(quant, im); % mean square error
-peaksnr = psnr(quant, im); % PSNR
-fprintf("Quality %f: err %f snr %f\n", Q, err, peaksnr);
+%% jpeg/inverse jpeg using quality 50 quantization matrix & constant equivalent
+huff = jpeg(im_uint8, qmat50);
+q50 = ijpeg(huff, qmat50);
+
+err = immse(q50, im_uint8); % mean square error
+peaksnr = psnr(q50, im_uint8); % PSNR
+fprintf("Quality 50: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,3);
+imshow(q50,'Border','tight');
+title(["Quality 50, SNR:" num2str(peaksnr)]);
+
+huff = jpeg(im_uint8, qmat_const_50);
+q_const_50 = ijpeg(huff, qmat_const_50);
+
+err = immse(q_const_50, im_uint8); % mean square error
+peaksnr = psnr(q_const_50, im_uint8); % PSNR
+fprintf("Const 50: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,4);
+imshow(q_const_50,'Border','tight');
+title(["Q50 constant equivalent, SNR:" num2str(peaksnr)]);
+
+
+%% jpeg/inverse jpeg using quality 98 quantization matrix & constant equivalent
+huff = jpeg(im_uint8, qmat98);
+q98 = ijpeg(huff, qmat98);
+
+err = immse(q98, im_uint8); % mean square error
+peaksnr = psnr(q98, im_uint8); % PSNR
+fprintf("Quality 98: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,5);
+imshow(q98,'Border','tight');
+title(["Quality 98, SNR:" num2str(peaksnr)]);
+
+huff = jpeg(im_uint8, qmat_const_98);
+q_const_98 = ijpeg(huff, qmat_const_98);
+
+err = immse(q_const_98, im_uint8); % mean square error
+peaksnr = psnr(q_const_98, im_uint8); % PSNR
+fprintf("Const 98: err %f snr %f\n", err, peaksnr);
+
+subplot(3,2,6);
+imshow(q_const_98,'Border','tight');
+title(["Q98 constant equivalent, SNR:" num2str(peaksnr)]);
 
 function im = ijpeg(huff, qmat)
     %retrieve matrix corresponding to concatenated 8x8 DCT blocks
@@ -97,4 +166,20 @@ function huff = jpeg(im, qmat)
     %encode blocks as huffman code
     %TEMPORARY: just return concatenated 8x8 DCT blocks
     huff = xq;
+end
+
+function qmat = make_qmat(Q, qmat)
+    % Determine S
+    if (Q < 50)
+        S = 5000/Q;
+    else
+        S = 200 - 2*Q;
+    end
+    
+    Tb = qmat;
+
+    Ts = floor((S*Tb + 50) / 100);
+    Ts(Ts == 0) = 1; % Prevent division by 0
+    
+    qmat = Ts;
 end
