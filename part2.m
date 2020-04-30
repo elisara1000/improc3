a62f4675f1fb7c8fe89670d5aaf15f68b46a0d49
Conor <conor.obrien24@gmail.com>
2020-04-30 02:56:33 +0100

    added different quantization matrices

--- part2.m
+++ part2.m
@@ -1,25 +1,85 @@
 close all; clear; clc;
 im = im2double(imread('cameraman.tif'));
 
+%%huffman test
+% symbols = (1:5); % Alphabet vector
+% prob = [.3 .3 .2 .1 .1]; % Symbol probability vector
+% [dict,avglen] = huffmandict(symbols,prob);
+
 %% JPEG
+im_uint8 = imread('cameraman.tif');
+
+figure
+subplot(1,5,1);
+imshow(im_uint8);
+title("original image");
+
+%% Define several quantization matrices
 %Quantization matrix specified in JPEG spec for 50% quality
-qmat = [16 11 10 16 24 40 51 61; 12 12 14 19 26 58 60 55;...
+qmat50 = [16 11 10 16 24 40 51 61; 12 12 14 19 26 58 60 55;...
     14 13 16 24 40 57 69 56; 14 17 22 29 51 87 80 62; ...
     18 22 37 56 68 109 103 77; 24 35 55 64 81 104 113 92;...
     49 64 78 87 103 121 120 101; 72 92 95 98 112 100 103 99];
 
-%jpeg/ijpeg function 
-im255 = imread('cameraman.tif');
-huff = jpeg(im255, qmat);
-q50 = ijpeg(huff, qmat);
+%make a higher quality quant matrix 
+qmat98 = round(rdivide(qmat50,2));
 
-figure
-subplot(1,2,1);
-imshow(im255);
-title("original image");
-subplot(1,2,2);
+qmat_const_50 = round(ones([8 8]) * (sum(qmat50(:))/numel(qmat50(:))));
+
+qmat_const_98 = round(qmat_const_50/2);
+
+
+%% jpeg/inverse jpeg using quality 50 quantization matrix 
+huff = jpeg(im_uint8, qmat50);
+q50 = ijpeg(huff, qmat50);
+
+err = immse(q50, im_uint8); % mean square error
+peaksnr = psnr(q50, im_uint8); % PSNR
+fprintf("Quality 50: err %f snr %f\n", err, peaksnr);
+
+subplot(1,5,2);
 imshow(q50);
 title("JPEG compressed image Quality 50");
+
+
+%% jpeg/inverse jpeg using quality 98 quantization matrix 
+huff = jpeg(im_uint8, qmat98);
+q98 = ijpeg(huff, qmat98);
+
+err = immse(q98, im_uint8); % mean square error
+peaksnr = psnr(q98, im_uint8); % PSNR
+fprintf("Quality 98: err %f snr %f\n", err, peaksnr);
+
+subplot(1,5,3);
+imshow(q98);
+title("JPEG compressed image Quality 98");
+
+
+%% jpeg/inverse jpeg using constant 50 quantization matrix 
+huff = jpeg(im_uint8, qmat_const_50);
+q_const_50 = ijpeg(huff, qmat_const_50);
+
+err = immse(q_const_50, im_uint8); % mean square error
+peaksnr = psnr(q_const_50, im_uint8); % PSNR
+fprintf("Const 50: err %f snr %f\n", err, peaksnr);
+
+subplot(1,5,4);
+imshow(q50);
+title("JPEG compressed image Const 50");
+
+
+%% jpeg/inverse jpeg using constant 50 quantization matrix 
+huff = jpeg(im_uint8, qmat_const_98);
+q_const_98 = ijpeg(huff, qmat_const_98);
+
+err = immse(q_const_98, im_uint8); % mean square error
+peaksnr = psnr(q_const_98, im_uint8); % PSNR
+fprintf("Const 98: err %f snr %f\n", err, peaksnr);
+
+subplot(1,5,5);
+imshow(q98);
+title("JPEG compressed image Const 98");
+
 
 pause;
 
