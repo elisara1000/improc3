759c7f06cf97409a1b26c17849100d6a2b2f891c
elisara1000 <elisara1000@hotmail.com>
2020-04-30 21:29:44 +0100

    hwt with rgb im

--- hwt.m
+++ hwt.m
@@ -1,24 +1,58 @@
 close all; clear; clc;
 
-im = im2double(imread('cameraman.tif'));
+%im = im2double(imread('cameraman.tif'));
+im = im2double(imread('peppers.png')); 
 
 % forward transform
 num_pass = 2;  % num of iterations
 h = im;
-W = hmat(size(im));
+[m, n, cnl] = size(im);
+W = hmat([m, n]);
 for pass = 1 : num_pass
-    h = W * h;
-    h = h * W.';    % = w*A*wT
+    h_r = W * h(:,: ,1) * W.';   % = w*A*wT
+    h_g = W * h(:,: ,2) * W.';
+    h_b = W * h(:,: ,3) * W.';
 end
-imshow(h);
+subplot(2,2, 1)
+imshow(h_r);
 
+subplot(2, 2, 2)
+imshow(h_g);
+
+subplot(2, 2, 3)
+imshow(h_b);
+
+h(:,:,1) = h_r;
+h(:,:,2) = h_g;
+h(:,:,3) = h_b;
+
+subplot(2, 2, 4)
+imshow(h);
 
 % inverse transform
 for pass = 1 : num_pass
-    h = pow2(num_pass)*W.'*h*W;
+    h_r = pow2(num_pass)*W.'*h(:,:,1)*W;
+    h_g = pow2(num_pass)*W.'*h(:,:,2)*W;
+    h_b = pow2(num_pass)*W.'*h(:,:,3)*W;
 end
 
-figure, imshow(h);
+figure;
+subplot(2,2, 1)
+imshow(h_r);
+
+subplot(2, 2, 2)
+imshow(h_g);
+
+subplot(2, 2, 3)
+imshow(h_b);
+
+h(:,:,1) = h_r;
+h(:,:,2) = h_g;
+h(:,:,3) = h_b;
+
+subplot(2, 2, 4)
+imshow(h);
+
 
 
 function Wn = hmat(N)
